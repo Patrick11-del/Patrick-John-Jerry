513f7f44c808c5506314ca3ea82d0b3819ed4858
Christian Patrick <christianpatrick1414@gmail.com>
2024-08-05 11:51:38 +0100

    Group Work

---
+++ pedro.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+read -p "what is your name" name
+read -p "what is the name of your state" state
+read -p "what is the name of your lga" lga
+read -p "what is the name of your primary school" primary school
+read -p "what is the name of your secondary school" secondary school
+echo "your name is $name and your are from $state and you are from $lga and your primary school is $primary $school and your secondary school is $secondary $school"
+
+
